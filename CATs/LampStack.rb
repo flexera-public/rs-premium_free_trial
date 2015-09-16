520976566b4aadade880a6b2acd992bc707d34c4
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2015-09-16 14:47:16 -0500

    changed path to blue code to use premium free trial repo and special branch

--- CATs/LampStack.rb
+++ CATs/LampStack.rb
@@ -45,8 +45,8 @@ parameter "param_appcode" do
   category "Application Code"
   label "Repository and Branch" 
   type "string" 
-  allowed_values "(Yellow) github.com/rightscale/examples:unified_php", "(Blue) github.com/MitchellGerdisch/demo_support_files:unified_php_modified" 
-  default "(Blue) github.com/MitchellGerdisch/demo_support_files:unified_php_modified"
+  allowed_values "(Yellow) github.com/rightscale/examples:unified_php", "(Blue) github.com/rs-services/rs-premium_free_trial:unified_php_modified" 
+  default "(Blue) github.com/rs-services/rs-premium_free_trial:unified_php_modified"
   operations "Update Application Code"
 end
 
