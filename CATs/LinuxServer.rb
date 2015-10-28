6a37285da14770114e20ab1204ad888b210ceed1
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2015-10-27 21:21:54 -0500

    Add SSH and RDP links to outputs

--- CATs/LinuxServer.rb
+++ CATs/LinuxServer.rb
@@ -68,7 +68,7 @@ end
 output "ssh_link" do
   label "SSH Link"
   category "Output"
-  description "Use this string along with your SSH key to access your server."
+  description "Use this string to access your server."
 end
 
 output "vmware_note" do
@@ -79,6 +79,7 @@ output "vmware_note" do
 end
 
 output "ssh_key_info" do
+  condition $inAzure
   label "Link to your SSH Key"
   category "Output"
   description "Use this link to download your SSH private key and use it to login to the server using provided \"SSH Link\"."
@@ -328,7 +329,10 @@ define enable(@linux_server, $inAzure, $invSphere) return $server_ip_address do
        @binding = select(@bindings, {"private_port":22})
        $server_ip_address = join(["-p ", @binding.public_port, " rightscale@", to_s(@linux_server.current_instance().public_ip_addresses[0])])
     else
-       $server_ip_address = join(["rightscale@", $server_addr])
+      # If not in Azure, then we can actually provide the SSH link like that found in CM.
+      call find_shard(@@deployment) retrieve $shard_number
+      call find_account_number() retrieve $account_number
+      call get_server_access_link(@linux_server, "SSH", $shard_number, $account_number) retrieve $server_ip_address
     end
 end 
 
@@ -362,6 +366,102 @@ define handle_retries($attempts) do
     sleep(60)
   else
     $_error_behavior = "skip"
+  end
+end
+
+# Helper Functions for creating the server access link provided back to the user
+# Returns either an RDP or SSH link for the given server.
+# This link can be provided as an output for a CAT and the user can select it to to get the
+# RDP or SSH file just like in Cloud Management.
+#
+# INPUTS:
+#   @server - server resource for which you want the link
+#   $link_type - "SSH" or "RDP" to indicate which type of access link you want back.
+#   $shard - the API shard to use. This can be found using the "find_shard.rb" definition.
+#   $account_number - the account number. This can be found using the "find_account_number.rb" definition.
+#
+define get_server_access_link(@server, $link_type, $shard, $account_number) return $server_access_link do
+  
+#  rs.audit_entries.create(notify: "None", audit_entry: { auditee_href: @@deployment, summary: join(["received account_number ", $account_number]), detail: ""})
+
+  $rs_endpoint = "https://us-"+$shard+".rightscale.com"
+    
+  $instance_href = @server.current_instance().href
+  
+  $response = http_get(
+    url: $rs_endpoint+"/api/instances",
+    headers: { 
+    "X-Api-Version": "1.6",
+    "X-Account": $account_number
+    }
+   )
+  
+  $instances = $response["body"]
+  
+  $instance_of_interest = select($instances, { "href" : $instance_href })[0]
+#  rs.audit_entries.create(notify: "None", audit_entry: { auditee_href: @@deployment, summary: join(["instance of interest"]), detail: to_s($instance_of_interest)})
+    
+  $legacy_id = $instance_of_interest["legacy_id"]  
+
+  $cloud_id = $instance_of_interest["links"]["cloud"]["id"]
+  
+  $instance_public_ips = $instance_of_interest["public_ip_addresses"]
+  $instance_private_ips = $instance_of_interest["private_ip_addresses"]
+  $instance_ip = switch(empty?($instance_public_ips), to_s($instance_private_ips[0]), to_s($instance_public_ips[0]))
+#  rs.audit_entries.create(notify: "None", audit_entry: { auditee_href: @@deployment, summary: join(["instance_ip: ", $instance_ip]), detail: ""})
+
+  $server_access_link_root = "https://my.rightscale.com/acct/"+$account_number+"/clouds/"+$cloud_id+"/instances/"+$legacy_id
+  
+  if $link_type == "RDP"
+    $server_access_link = $server_access_link_root +"/rdp?host=" + $instance_ip
+  elsif $link_type == "SSH"
+    $server_access_link = $server_access_link_root +"/managed_ssh.jnlp?host=" + $instance_ip
+  else
+    raise "Incorrect link_type, " + $link_type + ", passed to get_server_access_link()."
+  end
+  
+#  rs.audit_entries.create(notify: "None", audit_entry: { auditee_href: @@deployment, summary: "access link", detail: $server_access_link})
+
+end
+
+# Returns the RightScale account number in which the CAT was launched.
+define find_account_number() return $rs_account_number do
+  $cloud_accounts = to_object(first(rs.cloud_accounts.get()))
+  @info = first(rs.cloud_accounts.get())
+  $info_links = @info.links
+  $rs_account_info = select($info_links, { "rel": "account" })[0]
+  $rs_account_href = $rs_account_info["href"]  
+    
+  $rs_account_number = last(split($rs_account_href, "/"))
+#  rs.audit_entries.create(notify: "None", audit_entry: { auditee_href: @@deployment, summary: "rs_account_number" , detail: to_s($rs_account_number)})
+end
+
+# Returns the RightScale shard for the account the given CAT is launched in.
+# It relies on the fact that when a CAT is launched, the resultant deployment description includes a link
+# back to Self-Service. 
+# This link is exploited to identify the shard.
+# Of course, this is somewhat dangerous because if the deployment description is changed to remove that link, 
+# this code will not work.
+# Similarly, since the deployment description is also based on the CAT description, if the CAT author or publisher
+# puts something like "selfservice-8" in it for some reason, this code will likely get confused.
+# However, for the time being it's fine.
+define find_shard(@deployment) return $shard_number do
+  
+  $deployment_description = @deployment.description
+  #rs.audit_entries.create(notify: "None", audit_entry: { auditee_href: @deployment, summary: "deployment description" , detail: $deployment_description})
+  
+  # initialize a value
+  $shard_number = "UNKNOWN"
+  foreach $word in split($deployment_description, "/") do
+    if $word =~ "selfservice-" 
+    #rs.audit_entries.create(notify: "None", audit_entry: { auditee_href: @deployment, summary: join(["found word:",$word]) , detail: ""}) 
+      foreach $character in split($word, "") do
+        if $character =~ /[0-9]/
+          $shard_number = $character
+          rs.audit_entries.create(notify: "None", audit_entry: { auditee_href: @@deployment, summary: join(["found shard:",$character]) , detail: ""}) 
+        end
+      end
+    end
   end
 end
 
--- CATs/WindowsServer.rb
+++ CATs/WindowsServer.rb
@@ -352,7 +352,10 @@ define enable(@windows_server, $inAzure) return $server_ip_address do
      @binding = select(@bindings, {"private_port":3389})
      $server_ip_address = join([to_s(@windows_server.current_instance().public_ip_addresses[0]),":",@binding.public_port])
   else
-     $server_ip_address = @windows_server.current_instance().public_ip_addresses[0]
+    # If not in Azure, then we can actually provide the SSH link like that found in CM.
+    call find_shard(@@deployment) retrieve $shard_number
+    call find_account_number() retrieve $account_number
+    call get_server_access_link(@windows_server, "RDP", $shard_number, $account_number) retrieve $server_ip_address
   end
 end 
 
@@ -418,6 +421,100 @@ define handle_retries($attempts) do
     sleep(60)
   else
     $_error_behavior = "skip"
+  end
+end
+
+# Returns either an RDP or SSH link for the given server.
+# This link can be provided as an output for a CAT and the user can select it to to get the
+# RDP or SSH file just like in Cloud Management.
+#
+# INPUTS:
+#   @server - server resource for which you want the link
+#   $link_type - "SSH" or "RDP" to indicate which type of access link you want back.
+#   $shard - the API shard to use. This can be found using the "find_shard.rb" definition.
+#   $account_number - the account number. This can be found using the "find_account_number.rb" definition.
+#
+define get_server_access_link(@server, $link_type, $shard, $account_number) return $server_access_link do
+  
+  $rs_endpoint = "https://us-"+$shard+".rightscale.com"
+    
+  $instance_href = @server.current_instance().href
+  
+  $response = http_get(
+    url: $rs_endpoint+"/api/instances",
+    headers: { 
+    "X-Api-Version": "1.6",
+    "X-Account": $account_number
+    }
+   )
+  
+  $instances = $response["body"]
+  
+  $instance_of_interest = select($instances, { "href" : $instance_href })[0]
+#  rs.audit_entries.create(notify: "None", audit_entry: { auditee_href: @server, summary: join(["instance of interest"]), detail: to_s($instance_of_interest)})
+    
+  $legacy_id = $instance_of_interest["legacy_id"]  
+
+  $cloud_id = $instance_of_interest["links"]["cloud"]["id"]
+  
+  $instance_public_ips = $instance_of_interest["public_ip_addresses"]
+  $instance_private_ips = $instance_of_interest["private_ip_addresses"]
+  $instance_ip = switch(empty?($instance_public_ips), to_s($instance_private_ips[0]), to_s($instance_public_ips[0]))
+#  rs.audit_entries.create(notify: "None", audit_entry: { auditee_href: @server, summary: join(["instance_ip: ", $instance_ip]), detail: ""})
+
+  $server_access_link_root = "https://my.rightscale.com/acct/"+$account_number+"/clouds/"+$cloud_id+"/instances/"+$legacy_id
+  
+  if $link_type == "RDP"
+    $server_access_link = $server_access_link_root +"/rdp?host=" + $instance_ip
+  elsif $link_type == "SSH"
+    $server_access_link = $server_access_link_root +"/managed_ssh.jnlp?host=" + $instance_ip
+  else
+    raise "Incorrect link_type, " + $link_type + ", passed to get_server_access_link()."
+  end
+  
+#  rs.audit_entries.create(notify: "None", audit_entry: { auditee_href: @server, summary: "access link", detail: $server_access_link})
+
+end
+
+
+# Returns the RightScale account number in which the CAT was launched.
+define find_account_number() return $rs_account_number do
+  $cloud_accounts = to_object(first(rs.cloud_accounts.get()))
+  @info = first(rs.cloud_accounts.get())
+  $info_links = @info.links
+  $rs_account_info = select($info_links, { "rel": "account" })[0]
+  $rs_account_href = $rs_account_info["href"]  
+    
+  $rs_account_number = last(split($rs_account_href, "/"))
+  #rs.audit_entries.create(notify: "None", audit_entry: { auditee_href: @deployment, summary: "rs_account_number" , detail: to_s($rs_account_number)})
+end
+  
+# Returns the RightScale shard for the account the given CAT is launched in.
+# It relies on the fact that when a CAT is launched, the resultant deployment description includes a link
+# back to Self-Service. 
+# This link is exploited to identify the shard.
+# Of course, this is somewhat dangerous because if the deployment description is changed to remove that link, 
+# this code will not work.
+# Similarly, since the deployment description is also based on the CAT description, if the CAT author or publisher
+# puts something like "selfservice-8" in it for some reason, this code will likely get confused.
+# However, for the time being it's fine.
+define find_shard(@deployment) return $shard_number do
+  
+  $deployment_description = @deployment.description
+  #rs.audit_entries.create(notify: "None", audit_entry: { auditee_href: @deployment, summary: "deployment description" , detail: $deployment_description})
+  
+  # initialize a value
+  $shard_number = "UNKNOWN"
+  foreach $word in split($deployment_description, "/") do
+    if $word =~ "selfservice-" 
+    #rs.audit_entries.create(notify: "None", audit_entry: { auditee_href: @deployment, summary: join(["found word:",$word]) , detail: ""}) 
+      foreach $character in split($word, "") do
+        if $character =~ /[0-9]/
+          $shard_number = $character
+          #rs.audit_entries.create(notify: "None", audit_entry: { auditee_href: @deployment, summary: join(["found shard:",$character]) , detail: ""}) 
+        end
+      end
+    end
   end
 end
 
