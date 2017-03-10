91916e00c9c183f8088ba83da08dbfea1a55e37c
Ryan J. Geyer <me@ryangeyer.com>
2017-03-10 15:42:18 -0800

    Revert changes to windows CAT MCI. Needs more love

--- CATs/WindowsServer.cat.rb
+++ CATs/WindowsServer.cat.rb
@@ -22,7 +22,7 @@
 # Required prolog
 name 'B) Corporate Standard Windows Server'
 rs_ca_ver 20160622
-short_description "![logo](https://s3.amazonaws.com/rs-pft/cat-logos/windows.png)
+short_description "![logo](https://s3.amazonaws.com/rs-pft/cat-logos/windows.png) 
 
 Get a Windows Server VM in a our supported public clouds."
 long_description "Allows you to select different windows server types and cloud and performance level you want.\n
@@ -61,20 +61,20 @@ parameter "param_instancetype" do
   like $parameters.param_instancetype
 end
 
-#parameter "param_username" do
+#parameter "param_username" do 
 #  category "User Inputs"
-#  label "Windows Username"
+#  label "Windows Username" 
 ##  description "Username (will be created)."
-#  type "string"
+#  type "string" 
 #  no_echo "false"
 #end
 
-#parameter "param_password" do
+#parameter "param_password" do 
 #  category "User Inputs"
-#  label "Windows Password"
-#  description "Minimum at least 8 characters and must contain at least one of each of the following:
-#  Uppercase characters, Lowercase characters, Digits 0-9, Non alphanumeric characters [@#\$%^&+=]."
-#  type "string"
+#  label "Windows Password" 
+#  description "Minimum at least 8 characters and must contain at least one of each of the following: 
+#  Uppercase characters, Lowercase characters, Digits 0-9, Non alphanumeric characters [@#\$%^&+=]." 
+#  type "string" 
 #  min_length 8
 #  max_length 32
 #  # This enforces a stricter windows password complexity in that all 4 elements are required as opposed to just 3.
@@ -82,7 +82,7 @@ end
 #  no_echo "true"
 #end
 
-parameter "param_costcenter" do
+parameter "param_costcenter" do 
   category "User Inputs"
   like $parameters.param_costcenter
 end
@@ -105,13 +105,13 @@ output "output_win_username" do
 end
 
 output "output_win_credname" do
-  label "Windows Password Credential Name"
+  label "Windows Password Credential Name" 
   category "Output"
   description "Name to Credential Storing Windows Password"
 end
 
 output "output_win_cred_uri" do
-  label "Windows Password Credential Link"
+  label "Windows Password Credential Link" 
   category "Output"
   description "Link to Credential Storing Windows Password"
 end
@@ -119,11 +119,11 @@ end
 ##############
 # MAPPINGS   #
 ##############
-mapping "map_cloud" do
+mapping "map_cloud" do 
   like $mappings.map_cloud
 end
 
-mapping "map_instancetype" do
+mapping "map_instancetype" do 
   like $mappings.map_instancetype
 end
 
@@ -134,7 +134,7 @@ mapping "map_st" do {
   },
 } end
 
-# The off-the-shelf ServerTemplate being used has a couple of different MCIs defined based on the cloud.
+# The off-the-shelf ServerTemplate being used has a couple of different MCIs defined based on the cloud.   
 mapping "map_mci" do {
   "Windows 2008R2" => {  # Same MCI for all 3 environments
     "AWS" => "Windows_Server_Datacenter_2008R2_x64",
@@ -176,8 +176,8 @@ end
 
 condition "inAzure" do
   like $conditions.inAzure
-end
-
+end 
+  
 
 ############################
 # RESOURCE DEFINITIONS     #
@@ -194,7 +194,7 @@ resource "windows_server", type: "server" do
   multi_cloud_image find(map($map_mci, $param_servertype, $param_location))
   instance_type map($map_instancetype, $param_instancetype, $param_location)
   ssh_key_href map($map_cloud, $param_location, "ssh_key")
-  security_group_hrefs map($map_cloud, $param_location, "sg")
+  security_group_hrefs map($map_cloud, $param_location, "sg")  
   placement_group_href map($map_cloud, $param_location, "pg")
 end
 
@@ -222,7 +222,7 @@ resource "placement_group", type: "placement_group" do
   condition $needsPlacementGroup
   like @common_resources.placement_group
 end
-
+ 
 ##################
 # Permissions    #
 ##################
@@ -234,7 +234,7 @@ end
 # OPERATIONS       #
 ####################
 
-operation "launch" do
+operation "launch" do 
   description "Launch the server"
   definition "pre_auto_launch"
 end
@@ -242,7 +242,7 @@ end
 operation "enable" do
   description "Get information once the app has been launched"
   definition "enable"
-
+  
   # Update the links provided in the outputs.
    output_mappings do {
      $output_win_username => $win_username,
@@ -262,8 +262,8 @@ end
 ##########################
 
 # Import and set up what is needed for the server and then launch it.
-define pre_auto_launch($map_cloud, $param_location, $map_st, $param_servertype, $map_mci, @windows_server) return @windows_server do
-
+define pre_auto_launch($map_cloud, $param_location, $map_st) do
+    
   # Need the cloud name later on
   $cloud_name = map( $map_cloud, $param_location, "cloud" )
 
@@ -271,48 +271,42 @@ define pre_auto_launch($map_cloud, $param_location, $map_st, $param_servertype, 
   # Since different PIB scenarios include different clouds, this check is needed.
   # It raises an error if not which stops execution at that point.
   call cloud_utilities.checkCloudSupport($cloud_name, $param_location)
-
+  
   # Find and import the server template - just in case it hasn't been imported to the account already
   call server_templates_utilities.importServerTemplate($map_st)
-
-  $mci_name = $map_mci[$param_servertype][$param_location]
-  $windows_server = to_object(@windows_server)
-  @st = rs_cm.get(href: $windows_server["fields"]["server_template_href"])
-  @mci = @st.multi_cloud_images(filter:["name=="+$mci_name])
-  $windows_server["fields"]["multi_cloud_image_href"] = @mci.href
-  @windows_server = $windows_server
 
 end
 
 define enable(@windows_server, $param_costcenter, $param_location) return $win_username, $credname, $cred_uri do
-
+  
   # Tag the servers with the selected project cost center ID.
   $tags=[join(["costcenter:id=",$param_costcenter])]
   rs_cm.tags.multi_add(resource_hrefs: @@deployment.servers().current_instance().href[], tags: $tags)
-
+    
   # Gather up the generated password and make a credential for it and let the user know where to find it.
   $instance_info = @windows_server.current_instance().get(view: "sensitive")
   $admin_password = $instance_info[0]["admin_password"]
   $credname = join(["CAT_WINDOWS_ADMIN_PASSWORD-",last(split(@@deployment.href,"/"))])
   @cred = rs_cm.credentials.create({"name":$credname, "value": $admin_password})
-
+  
   $cred_num = last(split(@cred.href, "/"))
   call account_utilities.find_account_number() retrieve $account_num
   $cred_uri = "https://my.rightscale.com/acct/"+$account_num+"/credentials/"+$cred_num
-
-  $win_username = "administrator"
-  if ($param_location == "AzureRM")
-    $win_username = "rsadministrator"
+  
+  $win_username = "administrator"  
+  if ($param_location == "AzureRM") 
+    $win_username = "rsadministrator" 
   end
 
-end
+end 
 
 # Delete the credential created for the windows password
 define pre_auto_terminate() do
-
+  
   # Delete the cred we created for the user-provided password
   $credname = join(["CAT_WINDOWS_ADMIN_PASSWORD-",last(split(@@deployment.href,"/"))])
   @cred=rs_cm.credentials.get(filter: [join(["name==",$credname])])
   @cred.destroy()
-
+  
 end
+
