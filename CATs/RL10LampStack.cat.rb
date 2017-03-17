b28c1dc1821ba581287963e77be413d5b9e28fa8
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2017-03-16 19:15:33 -0500

    Changed name of LAMP stack to be C) so it follows Linux and Windows CATs which are currently A and B

--- CATs/RL10LampStack.cat.rb
+++ CATs/RL10LampStack.cat.rb
@@ -22,7 +22,7 @@
 # User can use a post-launch action to install a different version of the application software from a Git repo.
 
 
-name 'F) LAMP Stack (with Chef Server)'
+name 'C) LAMP Stack (with Chef Server)'
 rs_ca_ver 20160622
 short_description "![logo](https://s3.amazonaws.com/rs-pft/cat-logos/lamp_icon.png)
 
