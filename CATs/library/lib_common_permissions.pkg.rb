dd3785f753761233fb5acbcc0ba4e28dba5efe75
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2017-03-17 22:37:05 -0500

    permissions pkg file

---
+++ CATs/library/lib_common_permissions.pkg.rb
@@ -0,0 +1,12 @@
+name "LIB - Common permissions"
+rs_ca_ver 20161221
+short_description "Permissions that should be declared to make the CAT has the permissions needed for calls to RS APIs."
+
+package "pft/permissions"
+
+# Pretty inclusive set of permissions.
+# TODO: group things down into smaller sets perhaps so admin isn't always needed, etc.
+permission "pft_permissions" do
+  resources "rs_cm.tags", "rs_cm.instances", "rs_cm.audit_entries", "rs_cm.credentials", "rs_cm.clouds", "rs_cm.sessions", "rs_cm.accounts", "rs_cm.publications"
+  actions   "rs_cm.*"
+end
